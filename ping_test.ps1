72b3346249a0b3f5dcb4aa0b8590476745872d48
deadboy18 <53135082+deadboy18@users.noreply.github.com>
2023-07-25 16:09:24 +0800

    Add files via upload
    This script defines a function called Test-Ping, which takes a target (hostname or IP address) as input and uses the Test-Connection cmdlet to perform a single ping to that target. If the ping is successful, it displays a message indicating that the ping was successful; otherwise, it displays a message indicating that the target is not reachable or the ping failed.
    
    The script then creates two arrays, one for hostnames and another for IP addresses. It iterates over each item in these arrays and calls the Test-Ping function to ping each hostname and IP address in turn.
    
    Save the script in a .ps1 file (e.g., ping_test.ps1), and then you can run the script by opening PowerShell and executing the script file like this:

---
+++ ping_test.ps1
@@ -0,0 +1,28 @@
+﻿function Test-Ping {
+    param(
+        [string]$Target
+    )
+
+    $pingResult = Test-Connection -ComputerName $Target -Count 1 -Quiet
+
+    if ($pingResult) {
+        Write-Host "$Target ping successful."
+    } else {
+        Write-Host "$Target not reachable or ping failed."
+    }
+}
+
+$hostnames = @("DESKTOP-7H9EC33", "DESKTOP-H1NGLGJ")
+$ipAddresses = @("10.3.13.109", "10.3.13.110")
+
+foreach ($hostname in $hostnames) {
+    Test-Ping -Target $hostname
+}
+
+foreach ($ip in $ipAddresses) {
+    Test-Ping -Target $ip
+}
+
+
+# Pause the script execution to keep the PowerShell window open
+Read-Host "Press Enter to exit..."
